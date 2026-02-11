b923d1513a345973a0dacbc2faa8b92097aaa800
LudioRex <pemle2007@gmail.com>
2026-02-11 18:50:53 -0500

    Initial fix for clock issues

--- opentrig/src/fpga/latch.v
+++ opentrig/src/fpga/latch.v
@@ -36,7 +36,7 @@ module latch (
         // if sample event is called, shift the active channels
         // into out
         if (sample_interrupt) begin
-            out <= current;
+            out <= current & 24'h00_00_03;
         end
     end
 endmodule
