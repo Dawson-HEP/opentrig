43424ef484d15465554ce81e56ef6d96b7df2a3d
LudioRex <pemle2007@gmail.com>
2026-02-19 15:32:04 -0500

    Added latching for variable coincidence window.

--- opentrig/src/fpga/conincidence.v
+++ opentrig/src/fpga/conincidence.v
@@ -12,14 +12,42 @@ module trigger_coincidence (
     input wire clk,
     output reg trigger
 );
-    reg [23:0] sync_0, sync_1;
-    wire [23:0] rising = sync_0 & ~sync_1;
+    reg [`COINCIDENCE_WINDOW_N_CYCLES:0] sr [23:0];
+    wire [23:0] current;
+    
+    reg [23:0] sync_1;
+    wire [23:0] rising = inputs_async & ~sync_1;
+
     reg counting;
     reg [7:0] count;
 
+    integer i;
+
     always @(posedge clk) begin
-        sync_0 <= inputs_async;
-        sync_1 <= sync_0;
+        sync_1 <= inputs_async;
+
+        for (i = 0; i < 24; i= i +1) begin
+            sr[i] <= {sr[i][`COINCIDENCE_WINDOW_N_CYCLES-1:0], rising[i]};
+
+            current[i] <= |sr[i];
+        end
+
+
+        // Reformulation of the if-statement below. Should be equivalent,but it
+        // appears to drop some cycles.
+
+        // if (count >= `COINCIDENCE_OUT_N_CYCLES - 1) begin
+        //     counting <= 0;
+        //     trigger <= 0;
+
+        //     count <= 0;
+        // end else if (counting) begin
+        //     count <= count + 1;
+        // end else if ((current & `COINCIDENCE_MASK) == `COINCIDENCE_MASK) begin
+        //     counting <= 1;
+        //     trigger <= 1;
+        // end
+
 
             if (counting) begin
                 count <= count + 1;
@@ -30,24 +58,10 @@ module trigger_coincidence (
 
                     count <= 0;
                 end
-            end else if ((rising & `COINCIDENCE_MASK) == `COINCIDENCE_MASK) begin
+            end else if ((current & `COINCIDENCE_MASK) == `COINCIDENCE_MASK) begin
                 counting <= 1;
                 trigger <= 1;
             end
-
-        // Works but we don't know why. Scary!!!
-        // if (counting) begin
-        //     count <= count + 1;
-
-        //     if (count == `COINCIDENCE_OUT_N_CYCLES) begin
-        //         counting <= 0;
-        //         trigger <= 0;
-        //         count <= 0;
-        //     end
-        // end 
-        // else if (|(rising)) begin  // We have no clue why this works. But it does.            counting <= 1;
-        //  s   trigger <= 1;
-        // end
     end
 
 endmodule
